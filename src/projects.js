7330db35db89e4f64dc0476056b3f910b65f8fb9
Howard Pifer <paradx34@outlook.com>
2023-11-30 20:40:36 -0500

    addtional template files added

---
+++ src/projects.js
@@ -0,0 +1,67 @@
+// Array of projects to be rendered on portfolio page
+
+export const projects = [
+  {
+    projectTitle: "Dude Where's My Show",
+    description:
+      "A client-side web application for searching the streaming availability of movies and tv shows. Find your next show here!",
+    imageLink:
+      "https://user-images.githubusercontent.com/120127903/249894349-0480baf8-f6cf-454a-9267-abb883327ba1.png",
+    liveLink: "https://eeast.github.io/Dude-Wheres-My-Show/",
+    gitHub: "https://github.com/rmessett15/Dude-Wheres-My-Show",
+  },
+  {
+    projectTitle: "Fusion Fables",
+    description:
+      "A full-stack web application established for writers to come together to harness the fusion of collaboration and community.",
+    imageLink:
+      "https://user-images.githubusercontent.com/120127903/250159501-c9b19908-7e37-480b-8126-3379d303a4ad.png",
+    liveLink: "http://www.fusionfables.com/",
+    gitHub: "https://github.com/rmessett15/Fusion-Fables",
+  },
+  {
+    projectTitle: "Tech Blog",
+    description:
+      "A full-stack web application where users can come create and interact with content related to tech.",
+    imageLink:
+      "https://user-images.githubusercontent.com/120127903/250159643-a51d0557-0f08-4826-a0f0-f8d802408584.png",
+    liveLink: "https://mvc-tech-blog15.herokuapp.com/",
+    gitHub: "https://github.com/rmessett15/MVC-Tech-Blog",
+  },
+  {
+    projectTitle: "Code Quiz",
+    description:
+      "A simple web-application where users can come test their knowledge about coding. Find out if you're up to the test.",
+    imageLink:
+      "https://user-images.githubusercontent.com/120127903/250159663-da19d91f-765b-460f-ba19-811feb8b94d6.png",
+    liveLink: "https://rmessett15.github.io/Coding-Quiz/",
+    gitHub: "https://github.com/rmessett15/Coding-Quiz",
+  },
+  {
+    projectTitle: "Weather Dashboard",
+    description:
+      "Using third-party and server-side apis to bring clients live weather updates and future forecasts from anywhere in the world.",
+    imageLink:
+      "https://user-images.githubusercontent.com/120127903/250159688-6d04a5b7-fe26-4dae-9380-dfa29258e05a.png",
+    liveLink: "https://rmessett15.github.io/Weather-Dashboard/",
+    gitHub: "https://github.com/rmessett15/Weather-Dashboard",
+  },
+  {
+    projectTitle: "My First Website",
+    description:
+      "The first website I designed and deployed to the worldwide web. My web development journey started right here!",
+    imageLink:
+      "https://user-images.githubusercontent.com/120127903/250159711-eb5907ee-46ca-4291-8a3d-89e8dd43df4f.png",
+    liveLink: "https://rmessett15.github.io/My-First-Website/",
+    gitHub: "https://github.com/rmessett15/My-First-Website",
+  },
+  {
+    projectTitle: "Schedaddy",
+    description:
+      "Your company's ultimate solution for modern employee scheduling. A full stack MERN application.",
+    imageLink:
+      "https://user-images.githubusercontent.com/120127903/256081146-e641ae45-88a0-404d-bed1-a8badc0f7637.png",
+    liveLink: "https://schedaddy-0dffdc6ab2cd.herokuapp.com/",
+    gitHub: "https://github.com/cmcnamara15/Schedaddy",
+  },
+];
